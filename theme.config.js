8650178a0150141f429a66c6cb27d860732172fd
Stephen Puthenpurackal <puthenpurackalstephen@gmail.com>
2023-01-22 15:35:43 -0600

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
+      <time>{YEAR}</time> © The Serving Church, Inc.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
