ac3eec59fa10bc5d767abf896d950da39d225e03
Stephen Puthenpurackal <datfoosteve@gmail.com>
2023-02-01 00:25:46 +0000

    okay i hope this works

--- theme.config.js
+++ theme.config.js
@@ -5,17 +5,7 @@ export default {
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © The Serving Church, Inc.
       <a href="/feed.xml">RSS</a>
-      <style jsx>{`
-        a {
-          float: right;
-        }
-        @media screen and (max-width: 480px) {
-          article {
-            padding-top: 2rem;
-            padding-bottom: 4rem;
-          }
-        }
-      `}</style>
+      <style jsx>{`a{float: right;} @media screen and (max-width: 480px) {article {padding-top: 2rem;padding-bottom: 4rem;}}`}</style>
     </small>
   )
 }
